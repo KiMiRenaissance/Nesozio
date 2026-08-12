61276f19c381de5b807c982643b70c63fd972586
KiMiRenaissance <milanoinrock01@gmail.com>
2026-08-13 00:55:18 +0700

    style navbar

--- js/navbar.js
+++ js/navbar.js
@@ -67,7 +67,7 @@ const Navbar = {
         dropdown.innerHTML = `<div style="padding: 1rem; text-align: center; color: var(--text-muted);">No products found for "${query}"</div>`;
       } else {
         dropdown.innerHTML = matches.map(p => `
-          <div class="search-item d-flex mt-3" onclick="window.location.href='./product/product.html?id=${p.id}'">
+          <div class="search-item d-flex mt-3" " onclick="window.location.href='./product/product.html?id=${p.id}'">
             <img src="${p.images[0]}" alt="${p.name}" class="col-2 m-1">
             <div>
               <h6>${p.name}</h6>
